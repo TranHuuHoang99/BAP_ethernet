61f9ae84380896f1cab107da84b069c70ab8fc25
Tran Huu Hoang <thhoang08091999@gmail.com>
2026-09-18 09:25:17 +0700

    change text and color for health checking

--- runtime_fsg_simulation/src/client/handler/ClientViewHandler.tsx
+++ runtime_fsg_simulation/src/client/handler/ClientViewHandler.tsx
@@ -17,8 +17,8 @@ export function HealthCheck() {
     const toggleExecuteBin = () => {
         globalClientRequestHandler.handleRequest(ComponentIndex_t.HEALTH_CHECK_FSG_RUNNING, !isRunning);
     };
-    const buttonText = isRunning ? "ON" : "OFF";
-    const buttonColor = isRunning ? "#4CAF50" : "#ffffff";
+    const buttonText = isRunning ? "OFF" : "ON";
+    const buttonColor = isRunning ? "#F44336" : "#ffffff";
     return (
         <div style={{
             fontFamily: 'Arial, sans-serif',
@@ -922,6 +922,7 @@ export function SeatClimateZR() {
     )
 }
 
+<<<<<<< Updated upstream
 export function AirCirculationManual() {
     const [air_pos, setAirPos] = React.useState<number>(0);
 
@@ -942,11 +943,49 @@ export function AirCirculationManual() {
         globalClientRequestHandler.handleRequest(
             ComponentIndex_t.MODIFY_AIR_CIRC_MANUAL,
             air_pos
+=======
+export function TirePressure() {
+    const [heat_val, setHeatVal] = React.useState<number>(0);
+    const [heat_state, setHeatState] = React.useState<number>(0);
+    const [ven_val, setVenVal] = React.useState<number>(0);
+    const [ven_state, setVenState] = React.useState<number>(0);
+
+    React.useEffect(() => {
+        const onDataComes = (_heat_val: number,
+                             _heat_state: number,
+                             _ven_val: number,
+                             _ven_state: number) =>
+        {
+            setHeatVal(_heat_val);
+            setHeatState(_heat_state);
+            setVenVal(_ven_val);
+            setVenState(_ven_state);
+        }
+        globalClientRequestHandler.subcribe(ComponentIndex_t.MODIFY_SEAT_CLIMATE_ZR, onDataComes);
+        return () => {
+            globalClientRequestHandler.unsubcribe(ComponentIndex_t.MODIFY_SEAT_CLIMATE_ZR);
+        }
+    }, []);
+
+    const heatValOption = Array.from({length:4}, (_, i) => i);
+    const heatStateOption = Array.from({length:3}, (_, i) => i);
+    const venValOption = Array.from({length:4}, (_, i) => i);
+    const venStateOption = Array.from({length:3}, (_, i) => i);
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        globalClientRequestHandler.handleRequest(
+            ComponentIndex_t.MODIFY_SEAT_CLIMATE_ZR,
+            heat_val,
+            heat_state,
+            ven_val,
+            ven_state
+>>>>>>> Stashed changes
         )
     };
     return (
         <form onSubmit={handleSubmit}>
             <div>
+<<<<<<< Updated upstream
                 <h4>AIR CIRCULATION MANUAL</h4>
                 <div style={{display: 'flex', gap: '20px'}}>
                     <div>
@@ -1059,6 +1098,59 @@ export function AirDistributionZR() {
                                 <option key={air_distr_val} value={air_distr_val}>
                                     {airDistrName[air_distr_val-1]} | {air_distr_val}
                                 </option>
+=======
+                <h4>SEAT CLIMATE ZONE RIGHT</h4>
+                <div style={{display: 'flex', gap: '20px'}}>
+                    <div>
+                        <div><label>HEAT VAL</label></div>
+                        <select
+                            id='heat-val-right-select'
+                            name='heat-val-right'
+                            value={heat_val.toString()}
+                            onChange={(e) => setHeatVal(Number(e.target.value))}
+                        >
+                            {heatValOption.map((heat_val) => (
+                                <option key={heat_val} value={heat_val}>{heat_val}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <div><label>HEAT STATE</label></div>
+                        <select
+                            id='heat-state-right-select'
+                            name='heat-state-right'
+                            value={heat_state.toString()}
+                            onChange={(e) => setHeatState(Number(e.target.value))}
+                        >
+                            {heatStateOption.map((heat_state) => (
+                                <option key={heat_state} value={heat_state}>{heat_state}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <div><label>VENTILATION VAL</label></div>
+                        <select
+                            id='ven-val-right-select'
+                            name='ven-val-right'
+                            value={ven_val.toString()}
+                            onChange={(e) => setVenVal(Number(e.target.value))}
+                        >
+                            {venValOption.map((ven_val_t) => (
+                                <option key={ven_val_t} value={ven_val_t}>{ven_val_t}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <div><label>VENTILATION STATE</label></div>
+                        <select
+                            id='ven-state-right-select'
+                            name='ven-state-right'
+                            value={ven_state.toString()}
+                            onChange={(e) => setHeatState(Number(e.target.value))}
+                        >
+                            {venStateOption.map((ven_state_t) => (
+                                <option key={ven_state_t} value={ven_state_t}>{ven_state_t}</option>
+>>>>>>> Stashed changes
                             ))}
                         </select>
                     </div>
